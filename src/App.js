72f238fc49117ef55fbf12e9e48b922ba01d346f
dhanyasrigrandhi <2100030130@kluniversity.in>
2023-12-10 21:17:08 +0530

    newsapi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
+import News from './News';
 
 function App() {
+
+let [articles,setArticles]=useState([]);
+let [category,setCategory]=useState("india")
+  useEffect(()=>{
+     fetch(`https://newsapi.org/v2/everything?q=${category}&from=2023-12-09&apiKey=aa5d473d14cf4b59af12f4ef64ec40dd`)
+     .then((response)=>response.json())
+     .then((news)=>{
+      setArticles(news.articles);
+     })
+     .catch((err)=>{
+      console.log(err);
+     })
+  },[category])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className='header'>
+        <h1>Indian News</h1>
+        <input type='text' onChange={(event)=>{
+          if(event.target.value!=="")
+          {
+          setCategory(event.target.value);
+          }else{
+            setCategory("india");
+          }
+        }}placeholder='Search News'></input>
+        </header>
+      <section className='news-articles'>
+        {
+          articles.length!==0?
+          articles.map((article)=>{
+            return(
+              <News article={article}/>
+            )
+          })
+          :
+          <h3>No news Found For Searched Text</h3>
+        }
+      
+      </section>
     </div>
   );
 }
---
+++ src/News.js
@@ -0,0 +1,22 @@
+export default function News(props){
+    return(
+        <div className="news">
+            <div className="news-img">
+                {
+                    props.article.urlToImage!==null?
+                    <img src={props.article.urlToImage}/>:
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBTEghTW2XOwmp-PbaaC76eKvC7jH61oZfDQ&usqp=CAU"/>
+                    
+                }
+                
+                </div>
+             <h1>{props.article.title}</h1>
+             <p>{props.article.description?.substring(0,100).concat("....")}<a href="{props.article.url}" target="_blank">Read more</a></p>   
+             
+             <div className="source">
+                <p>Author: {props.article.author}</p>
+                <p>{props.article.source.name}</p>
+             </div>
+        </div>
+    )
+}
